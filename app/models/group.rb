89584bc5ba6c21d2f70668ad5a365df05f1b9a82
AshakaE <ashakaegerega@gmail.com>
2021-03-07 12:29:29 +0100

    create group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :projects
+
+  has_one_attached :icon
+
+  #validates :icon, attached: true, content_type: %i[png jpg jpeg]
+  validates :name, presence: true, uniqueness: true
 end
