b7686e0251b03e4c328fc430464ee01dada78ca7
AshakaE <ashakaegerega@gmail.com>
2021-03-07 11:37:15 +0100

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,28 @@
+ let environment = {
+  plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    }),
+  ]
+}
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    })
+  );
+}
+module.exports = environment
