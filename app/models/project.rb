16e2dfe872e9ac43feb5df3807d9fe55242a3510
AshakaE <ashakaegerega@gmail.com>
2021-03-07 16:50:19 +0100

    create project controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,7 @@ class Project < ApplicationRecord
   validates :name, presence: true, length: { in: 4..18 }
   validates :description, presence: true, length: { in: 10..160 }
   validates :amount, presence: true, numericality: true
+
+  scope :ug_incl, -> { includes(:user, :group) }
+  scope :desc, -> { order('created_at DESC') }
 end
