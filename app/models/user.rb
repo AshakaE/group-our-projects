af302cf859fd71dedff6309eb2940a98fbd45d3d
AshakaE <ashakaegerega@gmail.com>
2021-03-07 12:23:16 +0100

    create user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :groups
+  has_many :projects
+
+  validates :name, presence: true, uniqueness: true
+  validates :name, length: { minimum: 4, maximum: 18 }
+
+  has_one_attached :icon
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'should have many groups' do
+      t = User.reflect_on_association(:groups)
+      expect(t.macro).to eq(:has_many)
+    end
+    it 'should have many projects' do
+      t = User.reflect_on_association(:projects)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
+
+  describe 'validations' do
+    subject do
+      described_class.new(name: 'Ashaka')
+    end
+
+    it 'is valid with valid name' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a length name < 4 or > 18 charactars' do
+      subject.name = 'Sam'
+      expect(subject).to_not be_valid
+    end
+  end
 end
