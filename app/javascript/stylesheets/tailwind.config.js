30bc930772f4f920d9513212f82c02b3f9a1f1f0
AshakaE <ashakaegerega@gmail.com>
2021-03-10 15:20:28 +0100

    add mobile queries to tailwind config

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -39,6 +39,28 @@ module.exports = {
       5: "95vh",
       full: "100vh",
     },
+    screens: {
+      'sp': "320px",
+      // => @media (min-width: 320px) { ... }
+
+      'mp': "480px",
+      // => @media (min-width: 480px) { ... }
+
+      'sm': "640px",
+      // => @media (min-width: 640px) { ... }
+
+      'md': "768px",
+      // => @media (min-width: 768px) { ... }
+
+      'lg': "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   variants: {
     extend: {
