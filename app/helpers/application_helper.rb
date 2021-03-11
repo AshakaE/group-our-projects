2d90fdf4559f79592f5d2d1627805e9ac8d1e5b5
AshakaE <ashakaegerega@gmail.com>
2021-03-11 20:58:02 +0100

    Removed conditional statements for flash messages in action view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_alive
+    if flash[:alert]
+      render(partial: 'shared/alert')
+    elsif flash[:notice]
+      render(partial: 'shared/notice')
+    elsif flash[:warning]
+      render(partial: 'shared/warning')
+    end
+  end
 end
