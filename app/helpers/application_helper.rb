02ee5e9674b621ee2724a9cb3cac35cf2f6bca9e
AshakaE <ashakaegerega@gmail.com>
2021-03-12 14:33:30 +0100

    Removed conditional logics and loops from action views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
+# rubocop:disable Layout/BlockAlignment
+# rubocop:disable Layout/LineLength
+# rubocop:disable Style/RedundantInterpolation
+
 module ApplicationHelper
   def show_alive
     if flash[:alert]
@@ -22,18 +26,82 @@ module ApplicationHelper
   end
 
   def navbar(name)
-    render(partial: 'layouts/nav') unless name == 'Home' || name == 'Profile'
+    render(partial: 'layouts/nav') unless %w[Home Profile].any? name
+  end
+
+  def navlink(*)
+    content = ''
+    content << if logged_in?
+                 "<a href='/users/ #{current_user.id}'>"
+               else
+                 "<a href='/'>"
+               end
+    content.html_safe
   end
 
   def show_project(project)
     content = ''
-    if project.group
-      content << "<img src=' #{project.group.icon} ' alt='' class='h-20 w-20 my-auto'>"
-    else 
-      content << "<div class='my-auto h-20 w-20 border rounded-md border-gray-300'>
+    content << if project.group
+                 "<img src=' #{project.group.icon} ' alt='' class='h-20 w-20 my-auto'>"
+               else
+                 "<div class='my-auto h-20 w-20 border rounded-md border-gray-300'>
                   <small class='block text-center mt-5 font-bold'>no group</small>
                   </div>"
+               end
+    content.html_safe
+  end
+
+  def new_group
+    content = ''
+    content << "#{link_to new_group_path do
+       'NEW GROUP'
+     end}"
+    content.html_safe
+  end
+
+  def new_project
+    content = ''
+    content << "#{link_to new_project_path do
+       'NEW PROJECT'
+     end}"
+    content.html_safe
+  end
+
+  def show_proj_group(project)
+    content = ''
+    content << if project.group
+                 "<img src='#{project.group.icon}' alt='' class='h-20 w-20 my-auto'>"
+               else
+                 "<div class='my-auto ml-2 h-28 w-28 border rounded-md border-gray-300'>
+                   <small class='block text-center mt-5 font-bold'>no group</small>
+                   </div>"
+               end
+    content.html_safe
+  end
+
+  def show_proj_name(project)
+    content = ''
+    if controller_name == 'groups' && action_name == 'show'
+      content << "<p class='font-bold mr-3 text-md'>
+                  <small class='font-medium mr-1'>
+                  created by </small>  #{project.user.name} </p>"
+    end
+    content.html_safe
+  end
+
+  def show_icons(ico)
+    content = ''
+    1.upto(20) do |i|
+      content << "<li class='p-4 m-1'>
+                  #{ico.radio_button :icon, @icons[i]['link'], class: 'focus:ring-secondary h-4 w-4 text-secondary border-gray-300'}"
+      content << "#{ico.label :icon, value: @icons[i]['link'], class: 'w-2 h-2 py-4' do
+                    image_tag(@icons[i]['link'], class: 'w-14 h-14')
+                  end}"
+      content << '</li>'
     end
     content.html_safe
   end
 end
+# rubocop:enable Style/RedundantInterpolation
+# rubocop:enable Layout/LineLength
+# rubocop:enable Layout/BlockAlignment
